d1e49dcf60cf39829de3a5f3ba4c2045e0d93a8c
SHARVARI68 <75483333+SHARVARI68@users.noreply.github.com>
2021-05-30 15:15:13 +0530

    Update 1estimate.py

--- 1estimate.py
+++ 1estimate.py
@@ -1,40 +1,30 @@
-# =============================================================================
-# 2020-01-21
-# programming challenge from SciPy Lecture Notes
-# section 1.2.3 Getting Started > The Python Language > Control Flow
-# https://scipy-lectures.org/intro/language/control_flow.html
-# compute pi using the Wallis formula
-# 
-# precision  iterations
-# 1e-03      786
-# 1e-04      7855
-# 1e-05      78541
-# 1e-06      785399
-# 1e-07      7851419
-# 
-# =============================================================================
 
-# get a high precision value of pi for reference
-from math import pi as math_pi
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+            
+    def test_high_iters(self):
+        for i in range(500, 600):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
 
-def wallacepi(precision=1e-03):
-    """compute pi using the Wallis formula"""
-    my_pi = 1
-    product = 1
-    i = 1
 
-    while abs(math_pi - my_pi) > precision:
-        product *= (4*i**2) / ((4*i**2)-1)
-        my_pi = product * 2
-        i += 1
+class TestMC(unittest.TestCase):
+    def test_randomness(self):
+        pi0 = monte_carlo(15000)
+        pi1 = monte_carlo(15000)
         
-    return [my_pi,precision,i]
+        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
-print('Wallace Pi calculation using default precision')
-print(wallacepi())
-print()
+        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
 
-print('Wallace Pi calculation for several specified degrees of precision')
-print('[pi, precision, iterations]')
-for p in [1e-03,1e-04,1e-05,1e-06]:
-    print(wallacepi(p))
+    def test_accuracy(self):
+        for i in range(500, 600):
+            pi = monte_carlo(i)
+            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+        
+    
+if __name__ == "__main__":
+    unittest.main()
